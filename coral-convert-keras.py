9fdaf4d1039fe229c65ef1402789ec53a58db83c
Simon Schmidt <simonschmidt@Simons-MBP.fritz.box>
2020-07-10 19:52:38 +0200

    fixed keras conversion

--- coral-convert-keras.py
+++ coral-convert-keras.py
@@ -1,7 +1,4 @@
-import onnx2keras
-from onnx2keras import onnx_to_keras
 import keras
-import onnx
 import torch
 import pytorchcv
 from pytorchcv import model_provider
@@ -28,24 +25,16 @@ def representative_data_gen():
     image = tf.expand_dims(image, 0)
     yield [image]
 
-def dlModel(modelName, outputONNX):
-  model = model_provider._models[modelName](pretrained=True)
-  # This is for standard image net
-  x = torch.randn(1, 3, 224, 224, requires_grad=True)
-  torch_out = model(x)
-  torch.onnx.export(model, x, outputONNX, export_params=True, input_names=['input'], output_names=['output'])
+def dlModel(IMG_SHAPE):
+  model = tf.keras.applications.VGG19(input_shape=IMG_SHAPE,
+                                              include_top=False, 
+                                              weights='imagenet')
+  model.trainable = False
+  return model
 
-def onnx2keras(inputONNX, outputKeras):
-  print("CONVERTING ONNX TO KERAS...")
-  model = onnx.load(inputONNX)
-  k_model = onnx_to_keras(model, ['input'])
-  keras.models.save_model(k_model,outputKeras ,overwrite=True,include_optimizer=True)
-  print("DONE CONVERTING ONNX TO KERAS!")
+def keras2tflite_quant(inputKeras, outputTFlite):
 
-def keras2tflite_quant(inputKeras, outputTFLite):
-  kerasModel = tf.keras.models.load_model(inputKeras)
-
-  converter = tf.lite.TFLiteConverter.from_keras_model(kerasModel)
+  converter = tf.lite.TFLiteConverter.from_keras_model(inputKeras)
   # This enables quantization
   converter.optimizations = [tf.lite.Optimize.DEFAULT]
   converter.target_spec.supported_types = [tf.int8]
@@ -63,16 +52,13 @@ def keras2tflite_quant(inputKeras, outputTFLite):
 
 
 def main():
-
   # Filenames:
-  onnxFilename = 'torch_vgg19.onnx'
-  kerasFilename = 'keras_vgg19.h5'
-  tfliteFilename = 'vgg19_tensorFLow.tflite'
+  tfliteFilename = 'vgg19_tensorFlow.tflite'
 
   # Conversion Pipeline:
-  dlModel('vgg19', onnxFilename)
-  onnx2keras(onnxFilename, kerasFilename)
-  keras2tflite_quant(kerasFilename,tfliteFilename)
+  IMG_SHAPE = (224, 224, 3)
+  kerasModel = dlModel(IMG_SHAPE)
+  keras2tflite_quant(kerasModel, tfliteFilename)
 
 
 if __name__ == "__main__":
