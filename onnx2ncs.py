3afc51772cb51aab7e4c1e1f1a381fdd4791ff1a
simnsdt! <simnsdt@vm>
2020-07-16 03:25:11 -0700

    fixed model download

--- onnx2ncs.py
+++ onnx2ncs.py
@@ -9,10 +9,16 @@ def prepare(modelName):
         if not os.path.isfile(filename):
             if modelName == "ResNet50":
                 subprocess.run(
-                    ["curl", "https://s3.amazonaws.com/download.onnx/models/opset_8/resnet50.tar.gz", "--output", "ResNet50.onnx"])
+                    ["curl", "https://s3.amazonaws.com/download.onnx/models/opset_8/resnet50.tar.gz", "--output", "resnet50.tar.gz"])
+                subprocess.run(["tar", "-xf", "resnet50.tar.gz"])
+                subprocess.run(["tar", "-xf", "vgg19.tar.gz"])
+                subprocess.run(["mv", "resnet50/model.onnx", "./ResNet50.onnx"])
+                subprocess.run(["rm", "-r", "resnet50/", "resnet50.tar.gz"])
             elif modelName == "VGG19":
-                subprocess.run(["curl", "https://s3.amazonaws.com/download.onnx/models/opset_8/vgg19.tar.gz", "--output", "VGG19.onnx" ])
-                
+                subprocess.run(["curl", "https://s3.amazonaws.com/download.onnx/models/opset_8/vgg19.tar.gz", "--output", "vgg19.tar.gz" ])
+                subprocess.run(["tar", "-xf", "vgg19.tar.gz"])
+                subprocess.run(["mv", "vgg19/model.onnx", "./VGG19.onnx"])
+                subprocess.run(["rm", "-r", "vgg19/", "vgg19.tar.gz"])
     filename = modelName + ".onnx"
     _dlModel()
     modelOptimizer = os.path.expandvars("$HOME/intel/openvino/deployment_tools/model_optimizer/mo.py")
@@ -23,5 +29,3 @@ def bench(modelName, device, batch_size):
     sample_jpg = "sample.jpg"
     classification =  os.path.expandvars("./ncs/classification_sample.py")
     subprocess.run(["python3", classification, "-m", model_xml, "-i", sample_jpg, "-d", device, "-b", str(batch_size)])
-
-
