11e483d273b809ea4162771179b9bd70584472a0
simnsdt! <simnsdt@vm>
2020-07-17 03:11:57 -0700

    fixed cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Delete models:
-find . -iname 'VGG19*' -type f | xargs rmi
+find . -iname 'VGG19*' -type f | xargs rm
 find . -iname 'ResNet50*' -type f | xargs rm
 
 # Delete results:
 find . -iname 'results*' -type f | xargs rm
 
 # Delete __pycache__:
-find . -iname '*pycache*' -type f | xargs rm
+find . -iname '*pycache*' | xargs "rm -r"
 
 
 
