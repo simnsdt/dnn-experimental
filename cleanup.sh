cef584682cb81ac4d11ba27f817ba9b10af0991f
simnsdt! <simnsdt@vm>
2020-07-17 04:02:35 -0700

    added cleanup config options

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Delete models:
-find . -iname 'VGG19*' -type f | xargs rm
-find . -iname 'ResNet50*' -type f | xargs rm
-
+DELETE_MODELS="false"
+DELETE_RESULTS="true"
+DELETE_CACHE="true"
 # Delete results:
-find . -iname 'results*' -type f | xargs rm
+if [ "$DELETE_RESULTS" = true ]; then
+	find . -iname 'results*' -type f | xargs rm -f
+fi
+# Delete  models
+if [ "$DELETE_MODELS" = true ]; then
+	find . -iname 'VGG19*' -type f | xargs rm -f
+	find . -iname 'ResNet50*' -type f | xargs rm -f
+fi
 
 # Delete python cache:
-find . -iname '*pycache*' | xargs rm -r
-find . -iname '*.pyc' | xargs rm
-
-
+if [ "$DELETE_CACHE" = true ]; then
+	find . -iname '*pycache*' | xargs rm -r -f
+	find . -iname '*.pyc' | xargs rm -f
+fi
+echo "CLEANUP DONE!"
